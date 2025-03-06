807c0ca988a1878024a956f27df092915dabdae8
natasciarufino <natasciarufino17@gmail.com>
2025-03-06 10:01:30 +0100

    update

--- react_app_1/src/main.jsx
+++ react_app_1/src/main.jsx
@@ -2,10 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
+import ReactDOM from "react-dom/client";
+import Login from './Login.jsx';
+import Register from './Register.jsx';
 
-createRoot(document.getElementById('root')).render(
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <Routes>
+      <Route path="/" element={<App />} />
+      {/* creo la rotta per la login */}
+      <Route path='login' element={<Login />} />
+      {/* creo la rotta per la registrazione */}
+      <Route path='register' element={<Register/>} />
+    </Routes>
+  </BrowserRouter>
+);
